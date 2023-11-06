e203b562081746407803c05515b7be1649adf0c0
William <n11044209@qut.edu.au>
2023-11-06 12:20:08 +1000

    fixed upload route

--- app/routes/wordlist.js
+++ app/routes/wordlist.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { upload, getAll } = require("../controllers/wordlist.controller");
 const { multerUpload } = require("../middleware/fileUpload");
 
-router.post("/upload/:name", multerUpload.single("file"), upload);
+router.post("/upload/:wordlist", multerUpload.single("file"), upload);
 router.get("/", getAll);
 
 module.exports = router;
